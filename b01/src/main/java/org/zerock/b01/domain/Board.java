c3d96f4a9b852049c4fa891cb0d53050dc6edd21
demian-m00n <130627928+demian-m00n@users.noreply.github.com>
2023-07-05 23:57:38 +0900

    Part 7

--- b01/src/main/java/org/zerock/b01/domain/Board.java
+++ b01/src/main/java/org/zerock/b01/domain/Board.java
@@ -1,8 +1,11 @@
 package org.zerock.b01.domain;
 
 import lombok.*;
+import org.hibernate.annotations.BatchSize;
 
 import javax.persistence.*;
+import java.util.HashSet;
+import java.util.Set;
 
 @Entity
 @Getter
@@ -24,8 +27,33 @@ public class Board extends BaseEntity{
     @Column(length = 50, nullable = false)
     private String writer;
 
+    @OneToMany(mappedBy = "board",
+    cascade = {CascadeType.ALL},
+    fetch = FetchType.LAZY,
+    orphanRemoval = true)
+    @Builder.Default
+    @BatchSize(size = 20)
+    private Set<BoardImage> imageSet = new HashSet<>();
+
     public void change(String title, String content){
         this.title = title;
         this.content = content;
+    }
+
+    public void addImage(String uuid,String fileName){
+        BoardImage boardImage = BoardImage.builder()
+                .uuid(uuid)
+                .fileName(fileName)
+                .board(this)
+                .ord(imageSet.size())
+                .build();
+        imageSet.add(boardImage);
+    }
+
+    public void clearImages(){
+
+        imageSet.forEach(boardImage -> boardImage.changeBoard(null));
+
+        this.imageSet.clear();
     }
 }
--- b01/src/main/java/org/zerock/b01/service/BoardServiceImpl.java
+++ b01/src/main/java/org/zerock/b01/service/BoardServiceImpl.java
@@ -7,10 +7,7 @@ import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 import org.zerock.b01.domain.Board;
-import org.zerock.b01.dto.BoardDTO;
-import org.zerock.b01.dto.BoardListReplyCountDTO;
-import org.zerock.b01.dto.PageRequestDTO;
-import org.zerock.b01.dto.PageResponseDTO;
+import org.zerock.b01.dto.*;
 import org.zerock.b01.repository.BoardRepository;
 
 import javax.transaction.Transactional;
@@ -28,7 +25,7 @@ public class BoardServiceImpl implements BoardService{
 
     @Override
     public Long register(BoardDTO boardDTO){
-        Board board = modelMapper.map(boardDTO , Board.class);
+        Board board = dtoToEntity(boardDTO);
 
         Long bno = boardRepository.save(board).getBno();
 
@@ -41,7 +38,7 @@ public class BoardServiceImpl implements BoardService{
 
         Board board = result.orElseThrow();
 
-        BoardDTO boardDTO = modelMapper.map(board , BoardDTO.class);
+        BoardDTO boardDTO = entityToDTO(board);
 
         return boardDTO;
     }
@@ -53,6 +50,15 @@ public class BoardServiceImpl implements BoardService{
         Board board = result.orElseThrow();
 
         board.change(boardDTO.getTitle(), boardDTO.getContent());
+
+        board.clearImages();
+
+        if (boardDTO.getFileNames() != null){
+            for (String fileName : boardDTO.getFileNames()){
+                String[] arr = fileName.split("_");
+                board.addImage(arr[0],arr[1]);
+            }
+        }
 
         boardRepository.save(board);
     }
@@ -91,6 +97,21 @@ public class BoardServiceImpl implements BoardService{
                 .searchWithReplyCount(types,keyword,pageable);
 
         return PageResponseDTO.<BoardListReplyCountDTO>wihAll()
+                .pageRequestDTO(pageRequestDTO)
+                .dtoList(result.getContent())
+                .total((int)result.getTotalElements())
+                .build();
+    }
+
+    @Override
+    public PageResponseDTO<BoardListAllDTO> listWithAll(PageRequestDTO pageRequestDTO){
+        String[] types = pageRequestDTO.getTypes();
+        String keyword = pageRequestDTO.getKeyword();
+        Pageable pageable = pageRequestDTO.getPageable("bno");
+
+        Page<BoardListAllDTO> result = boardRepository.searchWithAll(types,keyword,pageable);
+
+        return PageResponseDTO.<BoardListAllDTO>wihAll()
                 .pageRequestDTO(pageRequestDTO)
                 .dtoList(result.getContent())
                 .total((int)result.getTotalElements())
